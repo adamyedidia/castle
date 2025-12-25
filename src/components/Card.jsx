f49c7605d0e68812a549f5bcea4d11da2a7afe8b
Adam Yedidia <adamyedidia@gmail.com>
2025-12-25 15:29:54 -0500

    visual improvements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,5 @@
 import React from 'react';
 
-const SUIT_SYMBOLS = {
-  diamonds: '♦',
-  spades: '♠',
-  hearts: '♥',
-  clubs: '♣',
-  joker: '★'
-};
-
-const SUIT_COLORS = {
-  diamonds: '#c41e3a',
-  hearts: '#c41e3a',
-  spades: '#1a1a2e',
-  clubs: '#1a1a2e',
-  joker: null // Determined by card color
-};
-
 export default function Card({ card, back, revealed, isSubmitted, disabled, small }) {
   // If no card data and not revealed, show card back
   if (!card && !revealed) {
@@ -28,7 +12,7 @@ export default function Card({ card, back, revealed, isSubmitted, disabled, smal
     );
   }
 
-  // Revealed card (shown face-up)
+  // Revealed card placeholder (shouldn't happen with proper data)
   if (!card && revealed) {
     return (
       <div className={`card card-revealed-back ${back === 'red' ? 'back-red' : 'back-blue'} ${small ? 'card-small' : ''}`}>
@@ -38,36 +22,16 @@ export default function Card({ card, back, revealed, isSubmitted, disabled, smal
   }
 
   const isJoker = card.rank === 'joker';
-  const suitSymbol = SUIT_SYMBOLS[card.suit] || '?';
-  const color = isJoker
-    ? (card.color === 'red' ? '#c41e3a' : '#1a1a2e')
-    : SUIT_COLORS[card.suit];
-
-  const displayRank = isJoker ? 'JOKER' : card.rank;
+  const isRed = card.color === 'red';
+  const displayText = isJoker ? 'Joker' : card.rank;
 
   return (
     <div
       className={`card card-front ${small ? 'card-small' : ''} ${isSubmitted ? 'submitted' : ''} ${disabled ? 'disabled' : ''}`}
-      style={{ '--card-color': color }}
     >
-      <div className="card-corner top-left">
-        <span className="rank">{displayRank}</span>
-        <span className="suit">{suitSymbol}</span>
-      </div>
-
-      <div className="card-center">
-        {isJoker ? (
-          <span className="joker-symbol">★</span>
-        ) : (
-          <span className="center-suit">{suitSymbol}</span>
-        )}
-      </div>
-
-      <div className="card-corner bottom-right">
-        <span className="rank">{displayRank}</span>
-        <span className="suit">{suitSymbol}</span>
-      </div>
+      <span className={`card-rank ${isRed ? 'rank-red' : 'rank-black'} ${isJoker ? 'rank-joker' : ''}`}>
+        {displayText}
+      </span>
     </div>
   );
 }
-
